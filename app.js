d9e228646b6298bca967cd7c0a80c0db512b8da2
SQZ777 <darren_zhang@trendmicro.com>
2020-12-23 17:32:22 +0800

    add flag and log, fix time calculate

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const { patchRequest, postRequest } = require('./lib/request');
 require('dotenv').config();
 
+let runFlag = true;
+
 async function cancelScooter(orderId) {
   requestBody = {
     action: 0,
@@ -31,27 +33,37 @@ async function orderScooter(scooterId) {
     headers: {
       'Content-Type': 'application/json',
       Authorization: process.env.Authorization,
+      'user-agent': 'GoShare/2.6.10 (iPhone; iOS 14.0; Scale/2.00)',
     },
     body: requestBody,
     json: true,
   });
   console.log(resp.body);
-  console.log(resp.body.id);
-  return resp.body.id;
+  if (resp.body.id) {
+    return resp.body.id;
+  }
+  runFlag = false;
+  return 'fail';
 }
 
 async function run() {
-  console.log('I am ordering');
-  const orderId = await orderScooter(process.env.ScooterId);
-  console.log('Order complete');
-  console.log('I am waiting...');
-  await new Promise((resolve) => setTimeout(resolve, 549000));
-  console.log('Wait complete, canceling order');
-  await cancelScooter(orderId);
-  console.log('Cancel complete.');
+  if (runFlag) {
+    console.log('I am ordering');
+    const orderId = await orderScooter(process.env.ScooterId);
+    console.log(`orderId: ${orderId}`);
+    if (orderId !== 'fail') {
+      console.log('Order complete');
+      console.log('I am waiting...');
+      await new Promise((resolve) => setTimeout(resolve, 578000));
+      console.log('Wait complete, canceling order');
+      await cancelScooter(orderId);
+      console.log('Cancel complete.');
+    }
+  }
+  console.log(`runFlag: ${runFlag}`);
 }
 
 run();
 setInterval(async () => {
   run();
-}, 550000);
+}, 581000);
